dc14cdccbeb144c912a2814d420418051c800515
tmasthay <tyler@ices.utexas.edu>
2023-02-25 20:28:53 +0100

    Add cut interior

---
+++ cut_interior.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def cut_cylinder(pts, p0, r0):
+    assert(np.linalg.norm(p0) > 0)
+    interior = []
+    boundary = []
+    for pt in pts:
+        alpha = np.dot(pt, p0) / np.linalg.norm(p0)
+        r = np.linalg.norm(pt - alpha * p0)
+        if( r < r0 ):
+            interior.append(pt)
+        else:
+            boundary.append(pt)
+    return interior, boundary
